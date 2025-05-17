7e58f020ff1ddbd71d98cc1a7fcdd03707e1a482
aapxapbect <mclovinkaden@gmail.com>
2025-05-16 20:29:45 -0400

    Update savemanager.lua

--- external/savemanager.lua
+++ external/savemanager.lua
@@ -3,8 +3,6 @@ local httpService = cloneref(game:GetService("HttpService"))
 local isfolder, isfile, listfiles = isfolder, isfile, listfiles
 
 if typeof(copyfunction) == "function" then
-    -- Fix is_____ functions for shitsploits, those functions should never error, only return a boolean.
-
     local
         isfolder_copy,
         isfile_copy,
@@ -81,6 +79,30 @@ local SaveManager = {} do
                 end
             end,
         },
+        KeyPicker = {
+            Save = function(idx, object)
+                local data = { type = "KeyPicker", idx = idx, mode = object.Mode, key = object.Value }
+                if object.Mode == "Toggle" then
+                    data.toggled = object.Toggled
+                end
+                return data
+            end,
+            Load = function(idx, data)
+                local object = SaveManager.Library.Options[idx]
+                if object then
+                    object:SetValue({ data.key, data.mode })
+                    if data.mode == "Toggle" and typeof(data.toggled) == "boolean" then
+                        object.Toggled = data.toggled
+                        if object.Callback then
+                             task.spawn(SaveManager.Library.SafeCallback, object.Callback, object.Toggled)
+                        end
+                        if object.Changed then
+                             task.spawn(SaveManager.Library.SafeCallback, object.Changed, object.Toggled)
+                        end
+                    end
+                end
+            end,
+        },
         Input = {
             Save = function(idx, object)
                 return { type = "Input", idx = idx, text = object.Value }
@@ -100,12 +122,11 @@ local SaveManager = {} do
 
     function SaveManager:IgnoreThemeSettings()
         self:SetIgnoreIndexes({
-            "BackgroundColor", "MainColor", "AccentColor", "OutlineColor", "FontColor", "FontFace", -- themes
-            "ThemeManager_ThemeList", "ThemeManager_CustomThemeList", "ThemeManager_CustomThemeName", -- themes
+            "BackgroundColor", "MainColor", "AccentColor", "OutlineColor", "FontColor", "FontFace",
+            "ThemeManager_ThemeList", "ThemeManager_CustomThemeList", "ThemeManager_CustomThemeName",
         })
     end
 
-    --// Folders \\--
     function SaveManager:CheckSubFolder(createFolder)
         if typeof(self.SubFolder) ~= "string" or self.SubFolder == "" then return false end
 
@@ -177,7 +198,6 @@ local SaveManager = {} do
         self:BuildFolderTree()
     end
 
-    --// Save, Load, Delete, Refresh \\--
     function SaveManager:Save(name)
         if (not name) then
             return false, "no config file is selected"
@@ -238,7 +258,7 @@ local SaveManager = {} do
             if not option.type then continue end
             if not self.Parser[option.type] then continue end
 
-            task.spawn(self.Parser[option.type].Load, option.idx, option) -- task.spawn() so the config loading wont get stuck.
+            task.spawn(self.Parser[option.type].Load, option.idx, option)
         end
 
         return true
@@ -279,8 +299,6 @@ local SaveManager = {} do
             for i = 1, #list do
                 local file = list[i]
                 if file:sub(-5) == ".json" then
-                    -- i hate this but it has to be done ...
-
                     local pos = file:find(".json", 1, true)
                     local start = pos
 
@@ -312,7 +330,6 @@ local SaveManager = {} do
         return data
     end
 
-    --// Auto Load \\--
     function SaveManager:GetAutoloadConfig()
         SaveManager:CheckFolderTree()
 
@@ -385,7 +402,6 @@ local SaveManager = {} do
         return true, ""
     end
 
-    --// GUI \\--
     function SaveManager:BuildConfigSection(tab)
         assert(self.Library, "Must set SaveManager.Library")
 
@@ -475,7 +491,6 @@ local SaveManager = {} do
 
         self.AutoloadLabel = section:AddLabel("Current autoload config: " .. self:GetAutoloadConfig(), true)
 
-        -- self:LoadAutoloadConfig()
         self:SetIgnoreIndexes({ "SaveManager_ConfigList", "SaveManager_ConfigName" })
     end
 
