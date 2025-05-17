a3f44f580afd4c182f0d993c60751c814c3a2509
aapxapbect <mclovinkaden@gmail.com>
2025-05-16 22:17:31 -0400

    Update premium.lua

--- external/premium.lua
+++ external/premium.lua
@@ -1,6 +1,5 @@
 local xapwarepremium = {
-    ["aapxapbect"] = {1421868474, 3369332661},
-    ["sigma"] = {12345},
+    ["aapxapbect"] = {1421868474, 3369332661, 8220929613},
 }
 
 return xapwarepremium
