db7f9c1fa63a2bd203a7e8063a37a12d5668ba4b
aapxapbect <mclovinkaden@gmail.com>
2025-05-24 22:04:10 -0400

    Add files via upload

---
+++ blacklist.lua
@@ -0,0 +1,12 @@
+local xapwareblacklist = {
+    users = {
+        1
+    },
+    games = {
+        10228136016,
+        3233893879,
+        292439477
+    }
+}
+
+return xapwareblacklist
---
+++ premium.lua
@@ -0,0 +1,6 @@
+local xapwarepremium = {
+    ["aapxapbect"] = {1421868474, 3369332661, 8221511004},
+    ["@zakuislame"] = {559088656},
+}
+
+return xapwarepremium
