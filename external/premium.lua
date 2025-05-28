e2b21e365f45d04d13ee54f9141f59072641731f
aapxapbect <mclovinkaden@gmail.com>
2025-05-28 02:10:37 -0400

    Update premium.lua

--- external/premium.lua
+++ external/premium.lua
@@ -1,6 +1,7 @@
 local xapwarepremium = {
     ["aapxapbect"] = {1421868474, 3369332661, 8221511004, 8546441933},
     ["@zakuislame"] = {559088656},
+    ["TheOneTrueGamer"] = {8510972069, 8510945957},
 }
 
 return xapwarepremium
