f7cdc3f0c628c6efe9cb1fd72cee2b29cb934328
aapxapbect <mclovinkaden@gmail.com>
2025-09-06 16:00:21 -0400

    Update broccoli.lua

--- external/broccoli.lua
+++ external/broccoli.lua
@@ -1,6 +1,5 @@
 return {
     xapbroccoli = {
-        ["noneyet"] = "Trident",
-        ["lol"] = "DaHood",
+        ["loltesting"] = "2788229376", -- 2788229376 is the secret word we will use for "Dahood" because 2788229376 is dahood's game id and we dont wanna just literally list dahood lol
     }
 }
